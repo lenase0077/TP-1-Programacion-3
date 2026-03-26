d2683a1229c0b7ac23a64377c2ad54027baa5a9b
Agus <agustinvalle23@gmail.com>
2026-03-26 10:54:36 +0000

    Crear UI ejercicio 3

--- Formulario Principal/Formulario Principal/Ejercicio 3.Designer.cs
+++ Formulario Principal/Formulario Principal/Ejercicio 3.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Ejercicio_3";
+            GroupBoxSexo = new GroupBox();
+            groupBox1 = new GroupBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            button1 = new Button();
+            checkedListBox1 = new CheckedListBox();
+            SuspendLayout();
+            // 
+            // GroupBoxSexo
+            // 
+            GroupBoxSexo.Location = new Point(80 ,51);
+            GroupBoxSexo.Name = "GroupBoxSexo";
+            GroupBoxSexo.Size = new Size(150 ,100);
+            GroupBoxSexo.TabIndex = 0;
+            GroupBoxSexo.TabStop = false;
+            GroupBoxSexo.Text = "Sexo";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(361 ,51);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(150 ,100);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Estado Civil";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(186 ,385);
+            button1.Name = "button1";
+            button1.Size = new Size(200 ,30);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(186 ,200);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(200 ,94);
+            checkedListBox1.TabIndex = 3;
+            // 
+            // Ejercicio_3
+            // 
+            AutoScaleDimensions = new SizeF(7F ,15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584 ,661);
+            Controls.Add(checkedListBox1);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(GroupBoxSexo);
+            Name = "Ejercicio_3";
+            Text = "Ejercicio_3";
+            FormClosed +=  Ejercicio_3_FormClosed ;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox GroupBoxSexo;
+        private GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button button1;
+        private CheckedListBox checkedListBox1;
     }
 }
--- Formulario Principal/Formulario Principal/Form Principal.Designer.cs
+++ Formulario Principal/Formulario Principal/Form Principal.Designer.cs
@@ -30,37 +30,51 @@
         {
             btnEjercicio_1 = new Button();
             buttonEjercicio2 = new Button();
+            buttonEjercicio3 = new Button();
             SuspendLayout();
             // 
             // btnEjercicio_1
             // 
             btnEjercicio_1.FlatStyle = FlatStyle.Flat;
-            btnEjercicio_1.Font = new Font("Segoe UI", 15F);
-            btnEjercicio_1.Location = new Point(89, 60);
+            btnEjercicio_1.Font = new Font("Segoe UI" ,15F);
+            btnEjercicio_1.Location = new Point(89 ,60);
             btnEjercicio_1.Name = "btnEjercicio_1";
-            btnEjercicio_1.Size = new Size(186, 62);
+            btnEjercicio_1.Size = new Size(186 ,62);
             btnEjercicio_1.TabIndex = 0;
             btnEjercicio_1.Text = "EJERCICIO 1";
             btnEjercicio_1.UseVisualStyleBackColor = true;
-            btnEjercicio_1.Click += btnEjercicio_1_Click;
+            btnEjercicio_1.Click +=  btnEjercicio_1_Click ;
             // 
             // buttonEjercicio2
             // 
             buttonEjercicio2.FlatStyle = FlatStyle.Flat;
-            buttonEjercicio2.Font = new Font("Segoe UI", 15F);
-            buttonEjercicio2.Location = new Point(301, 60);
+            buttonEjercicio2.Font = new Font("Segoe UI" ,15F);
+            buttonEjercicio2.Location = new Point(301 ,60);
             buttonEjercicio2.Name = "buttonEjercicio2";
-            buttonEjercicio2.Size = new Size(186, 62);
+            buttonEjercicio2.Size = new Size(186 ,62);
             buttonEjercicio2.TabIndex = 1;
             buttonEjercicio2.Text = "EJERCICIO 2";
             buttonEjercicio2.UseVisualStyleBackColor = true;
-            buttonEjercicio2.Click += buttonEjercicio2_Click;
+            buttonEjercicio2.Click +=  buttonEjercicio2_Click ;
+            // 
+            // buttonEjercicio3
+            // 
+            buttonEjercicio3.FlatStyle = FlatStyle.Flat;
+            buttonEjercicio3.Font = new Font("Segoe UI" ,15F);
+            buttonEjercicio3.Location = new Point(516 ,60);
+            buttonEjercicio3.Name = "buttonEjercicio3";
+            buttonEjercicio3.Size = new Size(186 ,62);
+            buttonEjercicio3.TabIndex = 2;
+            buttonEjercicio3.Text = "EJERCICIO 3";
+            buttonEjercicio3.UseVisualStyleBackColor = true;
+            buttonEjercicio3.MouseClick +=  buttonEjercicio3_MouseClick ;
             // 
             // FormularioPrincipal
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F ,15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800 ,450);
+            Controls.Add(buttonEjercicio3);
             Controls.Add(buttonEjercicio2);
             Controls.Add(btnEjercicio_1);
             Name = "FormularioPrincipal";
@@ -72,5 +86,6 @@
 
         private Button btnEjercicio_1;
         private Button buttonEjercicio2;
+        private Button buttonEjercicio3;
     }
 }
